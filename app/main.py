7d30b0bcfbb0dc192204f8f87e9b55ce189e4ee1
sunmyeonglee <03leesun@gmail.com>
2025-06-16 08:19:42 +0900

    fix: Edit uvicorn.run

--- app/main.py
+++ app/main.py
@@ -145,5 +145,11 @@ async def read_item(item_id: int):
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080)),
+        reload=False,
+    )
