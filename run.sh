c727993998b9e846ba62d712120254af8449cf3f
sunmyeonglee <03leesun@gmail.com>
2025-06-16 17:49:11 +0900

    fix: Remove 8080

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8080} --no-reload
+uvicorn app.main:app --host 0.0.0.0 --port ${PORT} --no-reload
